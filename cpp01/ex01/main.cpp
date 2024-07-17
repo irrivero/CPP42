cffad0d507acc73b25900ab925af95ce020c0747
Irene Rivero Casal <irivero-@c4b10c5.42berlin.de>
2024-07-17 15:25:46 +0200

    prototype in headerfile

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irivero- <irivero-@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:17:11 by irivero-          #+#    #+#             */
-/*   Updated: 2024/07/01 17:43:36 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:58:06 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-
-Zombie*	zombieHorde(int N, std::string name);
 
 int	main(void)
 {
