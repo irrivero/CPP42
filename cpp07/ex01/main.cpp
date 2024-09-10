47210df04c01e4dc84e2a7f9357752e59014a569
Irene Rivero Casal <irivero-@c3a4c5.42berlin.de>
2024-09-10 16:16:10 +0200

    cpp07 added cpp files from eval

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:41:05 by irivero-          #+#    #+#             */
-/*   Updated: 2024/09/10 16:03:42 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/09/10 16:13:35 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,35 +86,35 @@ int main()
     return 0;
 }
 
-// eval test
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
+// // eval test
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
 
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
-}
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
 
-template< typename T >
-void print( T& x )
-{
-  std::cout << x << std::endl;
-  return;
-}
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
 
-int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
 
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
 
-  return 0;
-}
+//   return 0;
+// }
