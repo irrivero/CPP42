4a221e299e3725be9f87a098354bf5db215af36e
Irene Rivero Casal <irivero-@c3a4c5.42berlin.de>
2024-09-10 12:37:54 +0200

    cpp07: ex02 testing complex types in main

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irivero- <irivero-@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:19:38 by irivero-          #+#    #+#             */
-/*   Updated: 2024/09/05 13:37:28 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/09/10 12:36:26 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 #include <iostream>
 #include <stdexcept>
+
+    struct Person{
+        std::string name;
+        int age;
+    };
 
 void testArray() 
 {
@@ -21,6 +26,8 @@ void testArray()
         for (unsigned int i = 0; i < arr1.size(); ++i)
             arr1[i] = i * 2;
 
+        std::cout << CYAN << "Array " << RESET << std::endl;
+        std::cout << std::endl;
         // Print elements
 		std::cout << CYAN << "PRINT	ELEMENTS " << RESET << std::endl;
         for (unsigned int i = 0; i < arr1.size(); ++i)
@@ -55,10 +62,41 @@ void testArray()
 	{
         std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
 	}
+    std::cout << std::endl;
+}
+
+void    testStruct()
+{
+    try {
+    std::cout << std::endl;
+    std::cout << CYAN << "Struct " << RESET << std::endl;
+    std::cout << std::endl;
+    Array<Person> arr(3);
+    arr[0].name = "Alice";
+    arr[0].age = 25;
+    arr[1].name = "Bob";
+    arr[1].age = 30;
+    arr[2].name = "Charlie";
+    arr[2].age = 35;
+
+    std::cout << CYAN << "Array of persons: " << RESET << std::endl;
+    for (unsigned int i = 0; i < arr.size(); ++i)
+        std::cout << "Name: " << arr[i].name << ", Age: " << arr[i].age << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Trying to access out of range index: " << std::endl;
+    std::cout << arr[5].name << std::endl;  // Should throw exception
+    }
+    catch (const std::out_of_range& e)
+    {
+        std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+    }
 }
 
 int main() 
 {
     testArray();
+    std::cout << "------------------------------------------" << std::endl;
+    testStruct();
     return 0;
 }
